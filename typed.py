18ff877db6ddeb50ac4d5b2abd66c0f0388a070f
sumedh kulkarni <sumedh14699@gmail.com>
2021-12-09 12:46:27 +0530

    merge child of batman who killed robine

--- typed.py
+++ typed.py
@@ -8,7 +8,7 @@ while True:
  if keyboard.is_pressed('e'):         
   while True:
         
-        k.type_string('i am son of batman!!!!')
+        k.type_string('i am son of batman!!!!, but i killed 2nd robine!!!!!!!!!!!!!!!!!!')
         
         k.tap_key(k.enter_key)
         

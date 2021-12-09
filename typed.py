ca4093e55228bd994b0d44aca2f960bcc7074667
sumedh kulkarni <sumedh14699@gmail.com>
2021-12-09 12:36:47 +0530

    u know.... the batgirl!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- typed.py
+++ typed.py
@@ -8,7 +8,7 @@ while True:
  if keyboard.is_pressed('e'):         
   while True:
         
-        k.type_string('harmonium....')
+        k.type_string('i am barbara!!!!!!!!!!!!!!!!!!!!')
         
         k.tap_key(k.enter_key)
         

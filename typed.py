ccde48ff9ba17b5306dec0a9678c471168ecdf51
sumedh kulkarni <sumedh14699@gmail.com>
2021-12-09 12:20:12 +0530

    another grander child changes

--- typed.py
+++ typed.py
@@ -8,7 +8,7 @@ while True:
  if keyboard.is_pressed('e'):         
   while True:
         
-        k.type_string('abba.... harmonium....')
+        k.type_string('harmonium....')
         
         k.tap_key(k.enter_key)
         
